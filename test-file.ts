0563da75bcd620700ae0feab87c82ca1ac7d6f1f
Trevor Sears <trevor@trevorsears.com>
2022-06-17 00:40:16 -0400

    Added actual testing documentation to `test-file.ts`.

--- test-file.ts
+++ test-file.ts
@@ -1,4 +1,15 @@
-export function myFunction(func: string, operand: number): float {
+/**
+ * Here's the JSDoc!! This is a very complex example of what a doc comment might
+ * look like.
+ * 
+ * This would be an extended description included in the primary body of the doc
+ * comment, but not part of the leading summary.
+ * 
+ * @param {string} func
+ * @param {number} operand
+ * @returns {float}
+ */
+export function myFunction(func: string, operand: number): number {
 	
 	return 0.00;
 	
