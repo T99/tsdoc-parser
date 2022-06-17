67a0eabd1a507b5584ddc7166334269022d54135
Trevor Sears <trevor@trevorsears.com>
2022-06-17 00:42:17 -0400

    Added a stub implementation of `TSDocDocumentationComment`.

---
+++ ts/doc-comments/tsdoc-documentation-comment.ts
@@ -0,0 +1,15 @@
+/*
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 10:33 PM -- June 16th, 2022
+ * Project: tsdoc-parser
+ */
+
+export class TSDocDocumentationComment {
+	
+	public static parse(docCommentString: string): TSDocDocumentationComment {
+		
+		return new TSDocDocumentationComment();
+		
+	}
+	
+}
